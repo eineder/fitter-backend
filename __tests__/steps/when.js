dda8cc6156becbdfb922845b84a8b302834452ef
max <max.eineder@googlemail.com>
2023-07-31 12:17:05 +0200

    Add e2e test

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const AWS = require('aws-sdk')
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
     const handler = require('../../functions/confirm-user-signup').handler
@@ -25,6 +26,37 @@ const we_invoke_confirmUserSignup = async (username, name, email) => {
     await handler(event, context)
 }
 
+/**
+ * Performs a real sign up of a new user using Cognito
+ */
+const a_user_signs_up = async (password, name, email) => {
+    const cognito = new AWS.CognitoIdentityServiceProvider()
+    const userPoolId = process.env.COGNITO_USER_POOL_ID
+    const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
+
+    const signUpResponse = await cognito.signUp({
+        ClientId: clientId,
+        Password: password,
+        Username: email,
+        UserAttributes: [
+            { Name: 'name', Value: name }
+        ]
+    }).promise()
+
+    const username = signUpResponse.UserSub
+    console.log(`User ${email} has signed up and has username ${username}`)
+
+    // Usually, a new user has to confirm his sign up by receiving a mail with confirmation link
+    // Here we confirm programmatically
+    await cognito.adminConfirmSignUp({
+        Username: username,
+        UserPoolId: userPoolId
+    }).promise()
+
+    console.log(`Confirmed sign up for user ${email}`)
+}
+
 module.exports = {
-    we_invoke_confirmUserSignup
+    we_invoke_confirmUserSignup,
+    a_user_signs_up
 }
