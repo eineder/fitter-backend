0ceff7d6e3511c2792090d005a1bd3598bae756e
max <max.eineder@googlemail.com>
2023-11-04 23:02:50 +0100

    Fix e2e tests with importing correct sdk packages and switching to node 18.x, which is required by SDK v3

--- __tests__/lib/cognitoUtil.js
+++ __tests__/lib/cognitoUtil.js
@@ -1,57 +1,54 @@
-require('dotenv').config()
+require("dotenv").config();
 const {
-    CognitoIdentityProvider
+  CognitoIdentityProvider,
 } = require("@aws-sdk/client-cognito-identity-provider");
 
 const signupAndConfirmUser = async (name, email, password) => {
-    const cognito = new CognitoIdentityProvider()
-    const userPoolId = process.env.COGNITO_USER_POOL_ID
-    const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
+  const cognito = new CognitoIdentityProvider();
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID;
 
-    const signUpResponse = await cognito.signUp({
-        ClientId: clientId,
-        Password: password,
-        Username: email,
-        UserAttributes: [
-            { Name: 'name', Value: name }
-        ]
-    })
+  const signUpResponse = await cognito.signUp({
+    ClientId: clientId,
+    Password: password,
+    Username: email,
+    UserAttributes: [{ Name: "name", Value: name }],
+  });
 
-    const username = signUpResponse.UserSub
-    console.log(`User ${email} has signed up and has username ${username}`)
+  const username = signUpResponse.UserSub;
+  console.log(`User ${email} has signed up and has username ${username}`);
 
-    // Usually, a new user has to confirm his sign up by receiving a mail with confirmation link
-    // Here we confirm programmatically
-    await cognito.adminConfirmSignUp({
-        Username: username,
-        UserPoolId: userPoolId
-    })
+  // Usually, a new user has to confirm his sign up by receiving a mail with confirmation link
+  // Here we confirm programmatically
+  await cognito.adminConfirmSignUp({
+    Username: username,
+    UserPoolId: userPoolId,
+  });
 
-    console.log(`Confirmed sign up for user ${username}`)
-    return { clientId, username }
-}
+  console.log(`Confirmed sign up for user ${username}`);
+  return { clientId, username };
+};
 
 const signInUser = async (clientId, username, password) => {
-    const cognito = new CognitoIdentityProvider()
-    const authToken = await cognito.initiateAuth({
-        ClientId: clientId,
-        AuthFlow: 'USER_PASSWORD_AUTH',
-        AuthParameters: {
-            USERNAME: username,
-            PASSWORD: password
-        }
-    })
+  const cognito = new CognitoIdentityProvider();
+  const authToken = await cognito.initiateAuth({
+    ClientId: clientId,
+    AuthFlow: "USER_PASSWORD_AUTH",
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
+    },
+  });
 
-    console.log(`[${username}] - signed in`)
+  console.log(`[${username}] - signed in`);
 
-    return {
-        idToken: authToken.AuthenticationResult.IdToken,
-        accessToken: authToken.AuthenticationResult.AccessToken
-    }
-}
-
+  return {
+    idToken: authToken.AuthenticationResult.IdToken,
+    accessToken: authToken.AuthenticationResult.AccessToken,
+  };
+};
 
 module.exports = {
-    signupAndConfirmUser,
-    signInUser
-}
+  signupAndConfirmUser,
+  signInUser,
+};
--- functions/tweet.js
+++ functions/tweet.js
@@ -1,70 +1,63 @@
-const ulid = require('ulid')
+const ulid = require("ulid");
+const { DynamoDB: DynamoDb } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 
-const {
-    DynamoDBDocument
-} = require("@aws-sdk/lib-dynamodb");
-
-const {
-    DynamoDB: DynamoDb
-} = require("@aws-sdk/client-dynamodb");
-
-const DocumentClient = DynamoDBDocument.from(new DynamoDb())
-const { TweetType } = require('../lib/constants')
-
-const { USERS_TABLE, TWEETS_TABLE, TIMELINES_TABLE } = process.env
+const DocumentClient = DynamoDBDocument.from(new DynamoDb());
+const { TweetType } = require("../lib/constants");
+const { USERS_TABLE, TWEETS_TABLE, TIMELINES_TABLE } = process.env;
 
 const tweet = async (event) => {
-    const { text } = event.arguments
-    const { username } = event.identity
-    const id = ulid.ulid()
-    const timestamp = new Date().toJSON()
+  const { text } = event.arguments;
+  const { username } = event.identity;
+  const id = ulid.ulid();
+  const timestamp = new Date().toJSON();
 
-    const newTweet = {
-        __typename: TweetType.TWEET,
-        id,
-        text,
-        creator: username,
-        createdAt: timestamp,
-        replies: 0,
-        likes: 0,
-        retweets: 0
-    }
+  const newTweet = {
+    __typename: TweetType.TWEET,
+    id,
+    text,
+    creator: username,
+    createdAt: timestamp,
+    replies: 0,
+    likes: 0,
+    retweets: 0,
+  };
 
-    await DocumentClient.transactWrite({
-        TransactItems: [
-            {
-                Put: {
-                    TableName: TWEETS_TABLE,
-                    Item: newTweet
-                }
-            },
-            {
-                Put: {
-                    TableName: TIMELINES_TABLE,
-                    Item: {
-                        userId: username,
-                        tweetId: id,
-                        timestamp
-                    }
-                }
-            },
-            {
-                Update: {
-                    TableName: USERS_TABLE,
-                    Key: {
-                        id: username
-                    },
-                    UpdateExpression: 'ADD tweetsCount :one',
-                    ExpressionAttributeValues: {
-                        ':one': 1
-                    },
-                    ConditionExpression: 'attribute_exists(id)'
-                }
-            }
-        ]
-    })
+  await DocumentClient.transactWrite({
+    TransactItems: [
+      {
+        Put: {
+          TableName: TWEETS_TABLE,
+          Item: newTweet,
+        },
+      },
+      {
+        Put: {
+          TableName: TIMELINES_TABLE,
+          Item: {
+            userId: username,
+            tweetId: id,
+            timestamp,
+          },
+        },
+      },
+      {
+        Update: {
+          TableName: USERS_TABLE,
+          Key: {
+            id: username,
+          },
+          UpdateExpression: "ADD tweetsCount :one",
+          ExpressionAttributeValues: {
+            ":one": 1,
+          },
+          ConditionExpression: "attribute_exists(id)",
+        },
+      },
+    ],
+  });
 
-    return newTweet
-}
+  return newTweet;
+};
 
-module.exports.handler = tweet
+module.exports.handler = tweet;
