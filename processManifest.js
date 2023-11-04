2f1b969b1e1ac481bece48c8ca308ea9a3f48ea6
max <max.eineder@googlemail.com>
2023-11-04 21:58:49 +0100

    Fix export of outputs to .env

--- processManifest.js
+++ processManifest.js
@@ -1,50 +1,61 @@
-const _ = require('lodash')
-const dotenv = require('dotenv')
-const fs = require('fs')
-const path = require('path')
-const { promisify } = require('util')
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 
 module.exports = async function processManifest(manifestData) {
-    const stageName = Object.keys(manifestData)
-    const { outputs } = manifestData[stageName]
+  const stageName = Object.keys(manifestData);
+  const { outputs } = manifestData[stageName];
 
-    const getOutputValue = (key) => {
-        console.log(`loading output value for [${key}]`)
-        const output = _.find(outputs, x => x.OutputKey === key)
-        if (!output) {
-            throw new Error(`No output found for ${key}`)
-        }
-        return output.OutputValue
-    }
+  const dotEnvFile = path.resolve(".env");
+  const keyValues = {};
+  for (const output of outputs) {
+    const key = camelToSnakeCase(output.OutputKey);
+    const value = output.OutputValue;
+    keyValues[key] = value;
+  }
 
-    const dotEnvFile = path.resolve('.env')
-    await updateDotEnv(dotEnvFile, {
-        API_URL: getOutputValue('AppsyncmasterclassGraphQlApiUrl'),
-    })
-}
+  await updateDotEnv(dotEnvFile, keyValues);
+};
 
 /* Utils, typically this would be a package includes from NPM */
 async function updateDotEnv(filePath, env) {
-    // Merge with existing values
-    try {
-        const existing = dotenv.parse(await promisify(fs.readFile)(filePath, 'utf-8'))
-        env = Object.assign(existing, env)
-    } catch (err) {
-        if (err.code !== 'ENOENT') {
-            throw err
-        }
+  // Merge with existing values
+  try {
+    const existing = dotenv.parse(
+      await promisify(fs.readFile)(filePath, "utf-8")
+    );
+    env = Object.assign(existing, env);
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
     }
+  }
 
-    const contents = Object.keys(env).map(key => format(key, env[key])).join('\n')
-    await promisify(fs.writeFile)(filePath, contents)
+  env = Object.keys(env)
+    .sort()
+    .reduce(function (accumulator, currentValue) {
+      return { ...accumulator, [currentValue]: env[currentValue] };
+    }, {});
 
-    return env
+  const contents = Object.keys(env)
+    .map((key) => format(key, env[key]))
+    .join("\n");
+  await promisify(fs.writeFile)(filePath, contents);
+
+  return env;
 }
 
 function escapeNewlines(str) {
-    return str.replace(/\n/g, '\\n')
+  return str.replace(/\n/g, "\\n");
 }
 
 function format(key, value) {
-    return `${key}=${escapeNewlines(value)}`
+  return `${key}=${escapeNewlines(value)}`;
+}
+
+function camelToSnakeCase(str) {
+  return str
+    .replace(/[A-Z]/g, (letter, index) => `${index > 0 ? "_" : ""}${letter}`)
+    .toUpperCase();
 }
