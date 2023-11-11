01e0535b4d725dda72c6834d777908855518d7ab
max <max.eineder@googlemail.com>
2023-11-11 22:22:34 +0100

    Set liked and retweeted when submitting a tweet.

--- functions/tweet.js
+++ functions/tweet.js
@@ -19,6 +19,8 @@ const tweet = async (event) => {
     replies: 0,
     likes: 0,
     retweets: 0,
+    liked: false,
+    retweeted: false,
   };
 
   const document = DynamoDBDocument.from(new DynamoDb());
