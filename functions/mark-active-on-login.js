c70649506df59b211e2c2efbead798d6a7b2f9be
max <max.eineder@googlemail.com>
2023-11-16 20:16:24 +0100

    Call lambda on login

--- functions/mark-active-on-login.js
+++ functions/mark-active-on-login.js
@@ -1,7 +1,12 @@
-const { log } = require("../shared/logger.js");
+const { log } = require("../lib/logger.js");
 
 module.exports.handler = async (event) => {
+  console.log("Login handler:", event);
+  console.log(" process.env.LOG_TABLE:", process.env.LOG_TABLE);
+
   log("Login handler:", event);
   //   if (event.triggerSource !== "PostAuthentication_ConfirmSignUp") {
   //   }
+
+  return event;
 };
