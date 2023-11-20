692e011eef9bd864bd676d579d8127ad73838c37
max <max.eineder@googlemail.com>
2023-11-20 13:27:59 +0100

    Remove log table, use cloud watch instead

--- functions/mark-active-on-login.js
+++ functions/mark-active-on-login.js
@@ -1,10 +1,7 @@
-const { log } = require("../lib/logger.js");
-
 module.exports.handler = async (event) => {
   console.log("Login handler:", event);
   console.log(" process.env.LOG_TABLE:", process.env.LOG_TABLE);
 
-  log("Login handler:", event);
   //   if (event.triggerSource !== "PostAuthentication_ConfirmSignUp") {
   //   }
 
