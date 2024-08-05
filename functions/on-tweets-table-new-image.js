449fbf3cf10a0b55855c060d1c3d68b4b97a18a3
eineder <49588296+eineder@users.noreply.github.com>
2024-08-05 13:44:52 +0200

    Add stage to event name

--- functions/on-tweets-table-new-image.js
+++ functions/on-tweets-table-new-image.js
@@ -6,8 +6,6 @@ const {
 } = require("@aws-sdk/client-eventbridge");
 
 module.exports.handler = async (event) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
-
   if (!event.Records || event.Records.length === 0)
     throw new Error("No records found in the event");
 
@@ -17,10 +15,28 @@ module.exports.handler = async (event) => {
     );
 
   const record = event.Records[0];
-  if (record.eventName !== "INSERT")
-    throw new Error(
-      `Only INSERT events are supported, received ${record.eventName}`
+  if (record?.dynamodb?.ApproximateCreationDateTime) {
+    const imageTime = new Date(
+      record.dynamodb.ApproximateCreationDateTime * 1000
     );
+    const differenceInMilliseconds = Date.now() - imageTime;
+    const differenceInMinutes = Math.floor(
+      differenceInMilliseconds / (1000 * 60)
+    );
+    if (differenceInMinutes > 10) {
+      console.log(
+        `The record is too old (${differenceInMinutes} minutes) - ignoring`
+      );
+      return event;
+    }
+  }
+  if (record.eventName !== "INSERT") {
+    console.log(
+      `Only INSERT events are supported, received ${record.eventName} - ignoring`
+    );
+    return event;
+  }
+  console.log("Received event:", JSON.stringify(event, null, 2));
 
   const fullTweet = unmarshall(record.dynamodb.NewImage);
   const detail = {
@@ -31,11 +47,15 @@ module.exports.handler = async (event) => {
     tweetsTableName: process.env.TWEETS_TABLE,
   };
   console.log("Detail:", JSON.stringify(detail, null, 2));
+
+  const stage = process.env.STAGE;
+  if (!stage) throw new Error("No STAGE environment variable set");
+
   const cmd = new PutEventsCommand({
     Entries: [
       {
         Source: "tweets.service",
-        DetailType: "new_tweet_posted",
+        DetailType: stage + "_new_tweet_posted",
         Detail: JSON.stringify(detail, 0, 2),
         EventBusName: "default",
       },
